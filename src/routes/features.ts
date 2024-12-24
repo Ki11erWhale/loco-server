09c1662891fdbc4fb72d85caf138f67e2f6fbebe
Ki11erWhale <killerwhale058@gmail.com>
2024-12-25 08:51:56 +0900

    Edit ip logger

--- src/routes/features.ts
+++ src/routes/features.ts
@@ -1,12 +1,20 @@
+import axios from 'axios';
 import express from 'express';
 import { ipLoggerService } from '../client/features/ip-logger-service';
 
 const featuresRouter = express.Router();
 
-featuresRouter.get('/ip-logger/:slug', (req, res) => {
-  res.redirect(
-    'https://github.com/user-attachments/assets/2d54d27d-dc4b-49c3-807a-7c6bce3023a6'
-  );
+featuresRouter.get('/ip-logger/:slug', async (req, res) => {
+  // res.redirect(
+  //   'https://github.com/user-attachments/assets/2d54d27d-dc4b-49c3-807a-7c6bce3023a6'
+  // );
+
+  const imageUrl =
+    'https://github.com/user-attachments/assets/2d54d27d-dc4b-49c3-807a-7c6bce3023a6';
+  const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+  res.set('Content-Type', response.headers['content-type']);
+  res.send(response.data);
 
   const [id, seed] = req.params.slug.split('-');
   const ip =
