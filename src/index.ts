3b13d46d9450182377f4ecd1a6e6d03b2ab93895
Ki11erWhale <killerwhale058@gmail.com>
2024-12-24 23:39:24 +0900

    Chg port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
